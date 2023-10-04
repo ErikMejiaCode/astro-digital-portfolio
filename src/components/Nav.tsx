82ea19536847dcd97c0109ab5f61bb0ddecb34b8
Erik Mejia <erikdojo352@gmail.com>
2023-10-04 17:15:22 -0400

    Nav - Hamburger menu

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,9 +1,11 @@
 import avatar from "../assets/images/avatar.png";
 import { useState } from "react";
 import { motion } from "framer-motion";
+import { useMediaQuery } from "../util/useMediaQuery";
 
 export default function Nav() {
   const [toggled, setToggled] = useState(false);
+  const matches = useMediaQuery("(min-width: 1280px)");
 
   return (
     <nav className="relative mx-8 mb-24 flex justify-between items-center pt-12 pb-6 font-medium md:mx-16 lg:mx-32">
@@ -28,15 +30,41 @@ export default function Nav() {
       </div>
 
       {/* Title */}
+
       <h1 className="text-lg font-bold">
         <a href="/">Hua.</a>
       </h1>
 
-      <div className="space-y-1">
-        <span className="block h-0.5 w-8 bg-black"></span>
-        <span className="block h-0.5 w-6 bg-black"></span>
-        <span className="block h-0.5 w-4 bg-black"></span>
-      </div>
+      {/* Check if  we are on mobile or not*/}
+
+      {matches && (
+        <div className="flex gap-12">
+          <a href="/">Home</a>
+          <a href="services">Services</a>
+          <a href="/contact">Contact</a>
+        </div>
+      )}
+
+      {!matches && (
+        <div
+          onClick={() => setToggled((prevToggle) => !prevToggle)}
+          className="space-y-1 cursor-pointer z-50"
+        >
+          <span className="block h-0.5 w-8 bg-black"></span>
+          <span className="block h-0.5 w-6 bg-black"></span>
+          <span className="block h-0.5 w-4 bg-black"></span>
+        </div>
+      )}
+
+      {toggled && !matches && (
+        <div className="fixed bg-white bottom-0 left-0 w-full h-screen flex items-center justify-center">
+          <div className="flex flex-col gap-24 text-lg">
+            <a href="/">Home</a>
+            <a href="services">Services</a>
+            <a href="/contact">Contact</a>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
